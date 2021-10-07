ab4356bad65832f408bafa305e1a3d7446f4935a
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-07 18:49:57 +0500

    Set interval

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const allImages = document.querySelectorAll(".image-container");
+const navButtons = document.querySelectorAll(".nav-buttons");
 const previousButton = document.querySelector(".left-button");
 const forwardButton = document.querySelector(".right-button");
 
-forwardButton.addEventListener("click", moveSlidesForward);
-previousButton.addEventListener("click", moveSlidesBackward);
+forwardButton.addEventListener("click", forwardClickHandler);
+previousButton.addEventListener("click", backwardClickHandler);
+
+let imageTimer = setInterval(autoSlideShow, 5000);
+
+function forwardClickHandler() {
+  moveSlidesForward();
+  moveNavButtonsForward();
+  clearInterval(imageTimer);
+  imageTimer = setInterval(autoSlideShow, 5000);
+}
+
+function backwardClickHandler() {
+  moveSlidesBackward();
+  moveNavButtonsBackward();
+  clearInterval(imageTimer);
+  imageTimer = setInterval(autoSlideShow, 5000);
+}
+
+function autoSlideShow() {
+  moveSlidesForward();
+  moveNavButtonsForward();
+}
 
 function moveSlidesForward() {
   for (let i = 0; i < allImages.length; i++) {
@@ -30,6 +52,36 @@ function moveSlidesBackward() {
       }
       allImages[i].classList.toggle("active");
       allImages[--i].classList.toggle("active");
+      return;
+    }
+  }
+}
+
+function moveNavButtonsForward() {
+  for (let i = 0; i < navButtons.length; i++) {
+    if (navButtons[i].classList.contains("active")) {
+      if (i === navButtons.length - 1) {
+        navButtons[i].classList.toggle("active");
+        navButtons[0].classList.toggle("active");
+        return;
+      }
+      navButtons[i].classList.toggle("active");
+      navButtons[++i].classList.toggle("active");
+      return;
+    }
+  }
+}
+
+function moveNavButtonsBackward() {
+  for (let i = 0; i < navButtons.length; i++) {
+    if (navButtons[i].classList.contains("active")) {
+      if (i === 0) {
+        navButtons[i].classList.toggle("active");
+        navButtons[navButtons.length - 1].classList.toggle("active");
+        return;
+      }
+      navButtons[i].classList.toggle("active");
+      navButtons[--i].classList.toggle("active");
       return;
     }
   }
